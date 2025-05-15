c5c2fd6b7daffda3686202e15d0f6b814fe2733e
Fatimagasimova <fatimagasimova05@gmail.com>
2025-05-15 23:41:08 +0400

    Commit mesajı2

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
 import { Routes, Route, Link,NavLink } from 'react-router-dom';
 import Stopwatch from './Components/Stopwatch';
-import Timer from './components/Timer';
-import Clock from './components/Clock';
+import Timer from './Components/Timer';
+import Clock from './Components/Clock';
 import "./App.css"
 
 function App() {
   return (
     <div className="app" style={{ textAlign: 'center', padding: '20px' }}>
       <nav style={{ marginBottom: '20px' }}>
-        <NavLink to='/'>Home</NavLink>
         <NavLink to="/clock">Fulltime</NavLink>
         <NavLink to="/stopwatch">Stopwatcher</NavLink>
         <NavLink to="/timer">Timer</NavLink>
       </nav>
 
       <Routes>
-        <Route path='/' element={<Home />} />
         <Route path="/stopwatch" element={<Stopwatch />} />
         <Route path="/timer" element={<Timer />} />
         <Route path="/clock" element={<Clock />} />
-        <Route path="*" element={<Home />} />
       </Routes>
     </div>
   );
